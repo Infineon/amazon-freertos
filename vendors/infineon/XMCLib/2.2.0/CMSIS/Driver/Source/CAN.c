dd66d1619be3482ef86b9a5b0941dc00f6c2d701
jferreir <jesus.ferreira@gmail.com>
2020-07-02 19:00:12 +0200

    declare resources as const

--- vendors/infineon/XMCLib/2.2.0/CMSIS/Driver/Source/CAN.c
+++ vendors/infineon/XMCLib/2.2.0/CMSIS/Driver/Source/CAN.c
@@ -129,7 +129,7 @@ static bool global_can_initialized = false;
 static volatile CAN_INFO_t CAN0_INFO;
 static XMC_CAN_MO_t CAN0_MO;
 
-static CAN_RESOURCES_t CAN0_Resources = 
+static const CAN_RESOURCES_t CAN0_Resources = 
 {
   {RTE_CAN0_TX_PORT},
   RTE_CAN0_TX_AF,
@@ -152,7 +152,7 @@ static CAN_RESOURCES_t CAN0_Resources =
 static volatile CAN_INFO_t CAN1_INFO;
 static XMC_CAN_MO_t CAN1_MO;
 
-static CAN_RESOURCES_t CAN1_Resources = 
+static const CAN_RESOURCES_t CAN1_Resources = 
 {
   {RTE_CAN1_TX_PORT},
   RTE_CAN1_TX_AF,
@@ -175,7 +175,7 @@ static CAN_RESOURCES_t CAN1_Resources =
 static volatile CAN_INFO_t CAN2_INFO;
 static XMC_CAN_MO_t CAN2_MO;
 
-static CAN_RESOURCES_t CAN2_Resources = 
+static const CAN_RESOURCES_t CAN2_Resources = 
 {
   {RTE_CAN2_TX_PORT},
   RTE_CAN2_TX_AF,
@@ -198,7 +198,7 @@ static CAN_RESOURCES_t CAN2_Resources =
 static volatile CAN_INFO_t CAN3_INFO;
 static XMC_CAN_MO_t CAN3_MO;
 
-static CAN_RESOURCES_t CAN3_Resources = 
+static const CAN_RESOURCES_t CAN3_Resources = 
 {
   {RTE_CAN3_TX_PORT},
   RTE_CAN3_TX_AF,
@@ -221,7 +221,7 @@ static CAN_RESOURCES_t CAN3_Resources =
 static volatile CAN_INFO_t CAN4_INFO;
 static XMC_CAN_MO_t CAN4_MO;
 
-static CAN_RESOURCES_t CAN4_Resources = 
+static const CAN_RESOURCES_t CAN4_Resources = 
 {
   {RTE_CAN4_TX_PORT},
   RTE_CAN4_TX_AF,
@@ -244,7 +244,7 @@ static CAN_RESOURCES_t CAN4_Resources =
 static volatile CAN_INFO_t CAN5_INFO;
 static XMC_CAN_MO_t CAN5_MO;
 
-static CAN_RESOURCES_t CAN5_Resources = 
+static const CAN_RESOURCES_t CAN5_Resources = 
 {
   {RTE_CAN5_TX_PORT},
   RTE_CAN5_TX_AF,
@@ -262,7 +262,7 @@ static CAN_RESOURCES_t CAN5_Resources =
 #endif
 
 /* CAN Resources */
-static CAN_RESOURCES_t *const can_resources[6] = 
+static const CAN_RESOURCES_t *const can_resources[6] = 
 {
 #if (RTE_CAN0 != 0)
   &CAN0_Resources,
