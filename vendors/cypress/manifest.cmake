9abe7f80ceb65055aecedf36196ff8edd0c8eeae
jferreir <jesus.ferreira@gmail.com>
2020-07-23 11:20:22 +0200

    Deleted vendors/projects not related to Infineon Added printf library

--- vendors/cypress/manifest.cmake
+++
@@ -1,9 +0,0 @@
-set(
-    AFR_MANIFEST_SUPPORTED_BOARDS
-    ""
-    CACHE INTERNAL "Supported boards list."
-)
-
-set(AFR_MANIFEST_BOARD_DIR "boards")
-set(AFR_MANIFEST_BOARD_DIR_cypress43 "boards/CYW943907AEVAL1F")
-set(AFR_MANIFEST_BOARD_DIR_cypress54 "boards/CYW954907AEVAL1F")
--- vendors/vendor/manifest.cmake
+++
@@ -1,7 +0,0 @@
-set(
-    AFR_MANIFEST_SUPPORTED_BOARDS
-    ""
-    CACHE INTERNAL "Supported boards list."
-)
-
-set(AFR_MANIFEST_BOARD_DIR "boards")
